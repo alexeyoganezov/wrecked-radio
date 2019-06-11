279f76023eb2e20e221250695b0dd2927f4120e5
Alexey Oganezov <oganezov.av@reliab.tech>
2019-06-11 15:48:01 +0300

    no message

--- src/Channel.ts
+++ src/Channel.ts
@@ -10,12 +10,12 @@
 /**
  * Shape of functions, that handles events.
  */
-type IEventHandler = <P, R>(payload: P) => R;
+type IEventHandler = <P, R>(payload?: P) => R;
 
 /**
  * Shape of functions, that handles request.
  */
-type IRequestHandler = <P, R>(payload: P) => R;
+type IRequestHandler = <P, R>(payload?: P) => R;
 
 /**
  * Shape of a list of registered event handlers.
